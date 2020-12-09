b59eaae5e809dde591f1e09fd690e77175684501
sushmitha <60508097+sushmithashiv24@users.noreply.github.com>
2020-12-09 22:28:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,167 @@
+import React, { useState } from "react";
+
+import "./styles.css";
+
+const movieDB = {
+  Tech: [
+    {
+      name: "The Matrix",
+      img:
+        "http://static.rogerebert.com/uploads/movie/movie_poster/the-matrix-1999/large_gynBNzwyaHKtXqlEKKLioNkjKgN.jpg",
+      description:
+        "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.",
+      year: "1999",
+      rating: "8.7"
+    },
+    {
+      name: "The Social Dilemma",
+      img: "https://miro.medium.com/max/2280/0*ol6OT3ik_LUTcXhA",
+      description:
+        "Explores the dangerous human impact of social networking, with tech experts sounding the alarm on their own creations.",
+      year: "2020",
+      rating: "7.7"
+    },
+    {
+      name: "The Terminator",
+      img:
+        "https://www.theterminatorfans.com/wp-content/uploads/2015/05/The-Terminator-Poster.jpg",
+      description:
+        "A human soldier is sent from 2029 to 1984 to stop an almost indestructible cyborg killing machine, sent from the same year, which has been programmed to execute a young woman whose unborn son is the key to humanity's future salvation.",
+      year: "1984",
+      rating: "8.0"
+    }
+  ],
+  SuperHero: [
+    {
+      name: "Wonder Woman",
+      img:
+        "https://cdn.traileraddict.com/content/warner-bros-pictures/wonder-woman-2017-5.jpg",
+      // img:"",
+      description:
+        "When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.",
+      year: "2017",
+      rating: "7.4"
+    },
+    {
+      name: "Deadpool",
+      img:
+        "https://mir-s3-cdn-cf.behance.net/project_modules/1400/02cc1333658190.56b2c8ea3720a.png",
+
+      description:
+        "A wisecracking mercenary gets experimented on and becomes immortal but ugly, and sets out to track down the man who ruined his looks.",
+      year: "2017",
+      rating: "8.0"
+    },
+    {
+      name: "The Wolverine",
+      img:
+        "http://media.comicbook.com/wp-content/uploads/2013/03/the-wolverine-poster.jpg",
+
+      description:
+        "A Wolverine comes to Japan to meet an old friend whose life he saved years ago, and gets embroiled in a conspiracy involving yakuza and mutants.",
+      year: "2013",
+      rating: "6.7"
+    }
+  ],
+  "TV Series": [
+    {
+      name: "Scam 1992: The Harshad Mehta Story ",
+      img:
+        "https://timesofindia.indiatimes.com/thumb/78585855.cms?width=219&height=317&quality=80&imgsize=26927",
+      description:
+        "Set in 1980's and 90's Bombay, it follows the life of Harshad Mehta, a stockbroker who took the stock market to dizzying heights and his catastrophic downfall.",
+      year: "2020",
+      rating: "9.5"
+    },
+    {
+      name: "Kota Factory",
+      img:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/d/d9/Kota_Factory_poster.jpg/230px-Kota_Factory_poster.jpg",
+      description:
+        "Dedicated to Shrimati SL Loney ji, Shri Irodov ji and Maanniya HC Verma ji, 'Kota Factory' is TVF's latest original. India's first 'Black and White' show highlights the problems present day IIT-JEE aspirants face in their day-to-day lives.",
+      year: "2019",
+      rating: "9.0"
+    },
+    {
+      name: "Panchayat",
+      img:
+        "https://m.media-amazon.com/images/M/MV5BMTllYzQ0MzctZTI4Ny00YTE2LTkzYjktMTc4ZWE2MDUxNzk2XkEyXkFqcGdeQXVyNzEyNTM4MTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+      description:
+        "A comedy-drama, which captures the journey of an engineering graduate Abhishek, who for lack of a better job option joins as secretary of a Panchayat office in a remote village of Uttar Pradesh.",
+      year: "2020",
+      rating: "8.7"
+    }
+  ]
+};
+
+export default function App() {
+  const genreArray = Object.keys(movieDB);
+  const [watchGenre, watchGenreSetter] = useState("Tech");
+
+  function watchDispalyer(movieType) {
+    watchGenreSetter(movieType);
+  }
+  // console.log(Object.values(movieDB[watchGenre]))
+
+  return (
+    <div className="App">
+      <nav>
+        <button disabled className="nav-container">
+          !IMDb
+        </button>
+      </nav>
+      <div id="box">
+        <h1 id="heading">It's Definitely !IMDb</h1>
+        <h2 id="subHeading">Genres </h2>
+
+        <div className="expand">
+          {genreArray.map((movieType) => (
+            <button
+              key={movieType}
+              className="clickable"
+              onClick={() => watchDispalyer(movieType)}
+            >
+              {movieType}
+            </button>
+          ))}
+        </div>
+        <div>
+          {Object.values(movieDB[watchGenre]).map((objc) => {
+            var name = objc.name;
+            var description = objc.description;
+            var year = objc.year;
+            var rating = objc.rating;
+            var img = objc.img;
+
+            return (
+              <div className="classBox" key={name}>
+                <img
+                  alt="poster"
+                  style={{
+                    // padding: "2.5vmin 3vmin",
+                    paddingRight: "1rem",
+                    width: "22vw",
+                    maxWidth: "200px"
+                  }}
+                  src={img}
+                />
+
+                <div>
+                  <h3 style={{ textAlign: "left" }}>{name}</h3> <br />
+                  <div
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <strong style={{ color: "yellow" }}>{rating} ⭐</strong>
+                    <strong>Year : {year}</strong>
+                  </div>
+                  <br />
+                  <p> {description} </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
